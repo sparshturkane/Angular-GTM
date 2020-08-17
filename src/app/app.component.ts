178b6ecda6f736bb754e9a5478360a867cbd984e
sparshturkane <rajaturkane99@gmail.com>
2020-08-17 16:19:18 +0530

    added GTM specific code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { filter } from 'rxjs/operators';
+declare let dataLayer;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-gtm-setup';
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+
+    if (isPlatformBrowser) {
+      this.router.events.pipe(
+        filter((event) => event instanceof NavigationEnd)
+      ).subscribe(x => {
+        window.scrollTo(0, 0);
+
+        if (x instanceof NavigationEnd) {
+
+          // Pushing Each page in Datalayer event
+          dataLayer.push({
+            'event': 'page',
+            'name': x.urlAfterRedirects
+          });
+        }
+
+      });
+    }
+
+  }
+
 }
